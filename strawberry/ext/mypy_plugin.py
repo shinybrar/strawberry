e91d325049fd05440e3f85e693acc6bbab9ab91a
Patrick Arminio <patrick.arminio@gmail.com>
2020-09-24 16:34:42 +0100

    Mypy

--- strawberry/ext/mypy_plugin.py
+++ strawberry/ext/mypy_plugin.py
@@ -12,6 +12,19 @@ def lazy_type_analyze_callback(ctx: AnalyzeTypeContext) -> Type:
     return type_
 
 
+def strawberry_pydantic_class_callback(ctx: ClassDefContext):
+    # in future we want to have a proper pydantic plugin, but for now
+    # let's fallback to any, some resources are here:
+    # https://github.com/samuelcolvin/pydantic/blob/master/pydantic/mypy.py
+    # >>> model_index = ctx.cls.decorators[0].arg_names.index("model")
+    # >>> model_name = ctx.cls.decorators[0].args[model_index].name
+
+    # >>> model_type = ctx.api.named_type("UserModel")
+    # >>> model_type = ctx.api.lookup(model_name, Context())
+
+    ctx.cls.info.fallback_to_any = True
+
+
 class StrawberryPlugin(Plugin):
     def get_type_analyze_hook(self, fullname: str):
         if fullname == "strawberry.lazy_type.LazyType":
@@ -32,6 +45,10 @@ class StrawberryPlugin(Plugin):
             }
         ):
             return dataclasses.dataclass_class_maker_callback
+
+        if "strawberry.pydantic.type" in fullname:
+            return strawberry_pydantic_class_callback
+
         return None
 
 
