d495d09cca10aa7165de4589892d906ab7e536a6
Patrick Arminio <patrick.arminio@gmail.com>
2020-10-03 22:07:52 +0100

    WIP

--- strawberry/dataloader.py
+++ strawberry/dataloader.py
@@ -1,10 +1,25 @@
 from asyncio.futures import Future
-from typing import Any, Callable
+from typing import Callable, Generic, List, TypeVar
 
 
-class Dataloader:
+T = TypeVar("T")
+K = TypeVar("K")
+
+
+class Dataloader(Generic[T, K]):
+    batch: List[Future] = []
+
     def __init__(self, load_fn: Callable):
         self.load_fn = load_fn
 
-    async def load(self, id: Any) -> Future:
-        ...
+    # TODO: how can make future generic here?
+    async def load(self, id: K) -> Future:
+        # if we have an entry in our cache, we should return the
+        # promise from there
+
+        # TODO: pass loop?
+        future: Future = Future()
+
+        self.batch.append(future)
+
+        return future
